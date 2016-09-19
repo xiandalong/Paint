6d6dbafa5c7f27f257c9ae009103ed9dac1ddf29
xiandalong <kelvinlong8897@gmail.com>
2016-09-19 16:53:06 -0400

    Test run: drawing circles

---
+++ app/src/main/java/com/example/xiandalong/paint/DrawingView.java
@@ -0,0 +1,42 @@
+package com.example.xiandalong.paint;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class DrawingView extends View {
+
+    private final int paintColor = Color.BLACK;
+    private Paint drawPaint;
+
+
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        setupPaint();
+    }
+
+    private void setupPaint() {
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(5);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawCircle(50, 50, 20, drawPaint);
+        drawPaint.setColor(Color.GREEN);
+        canvas.drawCircle(50, 150, 20, drawPaint);
+        drawPaint.setColor(Color.BLUE);
+        canvas.drawCircle(50, 250, 20, drawPaint);
+    }
+}
